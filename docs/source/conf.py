57fa236b5dd6494e61668c60c3c61ae6c4dc7288
Tadej Borovšak <tadej.borovsak@xlab.si>
2019-10-20 16:28:20 +0200

    Initialize Sphinx documentation builder
    In order to start building the documentation, we bootstrapped the
    initial directory structure that Sphinx needs. As promised, we reused
    the README.rst file as an entry point.
    
    To install prerequisites, run
    
        $ pip install -r docs.requirements
    
    Documentation can now be built by running
    
        $ cd docs && make html
    
    This will place the built documentation into the docs/build/html
    directory.

---
+++ docs/source/conf.py
@@ -0,0 +1,10 @@
+project = "Sensu Go Ansible Collection"
+copyright = "2019, XLAB Steampunk"
+author = "XLAB Steampunk"
+
+extensions = [
+    "sphinx_rtd_theme",
+]
+exclude_patterns = []
+
+html_theme = "sphinx_rtd_theme"
