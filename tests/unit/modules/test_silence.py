a5ee31ced6d2a10af6b14e6b66f663ebc1e27188
Aljaž Košir <aljazkosir5@gmail.com>
2019-10-22 09:52:56 +0200

    Update silence module

---
+++ tests/unit/modules/test_silence.py
@@ -0,0 +1,113 @@
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+import pytest
+
+from ansible_collections.sensu.sensu_go.plugins.module_utils import (
+    errors, utils,
+)
+from ansible_collections.sensu.sensu_go.plugins.modules import silence
+
+from .common.utils import (
+    AnsibleExitJson, AnsibleFailJson, ModuleTestCase, set_module_args,
+)
+
+
+class TestSilence(ModuleTestCase):
+    def test_minimal_silence_parameters_check(self, mocker):
+        sync_mock = mocker.patch.object(utils, 'sync')
+        sync_mock.return_value = True, {}
+        set_module_args(
+            check='check'
+        )
+
+        with pytest.raises(AnsibleExitJson):
+            silence.main()
+
+        state, _client, path, payload, check_mode = sync_mock.call_args[0]
+        assert state == 'present'
+        assert path == '/silenced/*:check'
+        assert payload == dict(
+            check='check',
+            metadata=dict(
+                name='*:check',
+                namespace='default',
+            ),
+        )
+        assert check_mode is False
+
+    def test_minimal_silence_parameters_subscription(self, mocker):
+        sync_mock = mocker.patch.object(utils, 'sync')
+        sync_mock.return_value = True, {}
+        set_module_args(
+            subscription='subscription'
+        )
+
+        with pytest.raises(AnsibleExitJson):
+            silence.main()
+
+        state, _client, path, payload, check_mode = sync_mock.call_args[0]
+        assert state == 'present'
+        assert path == '/silenced/subscription:*'
+        assert payload == dict(
+            subscription='subscription',
+            metadata=dict(
+                name='subscription:*',
+                namespace='default',
+            ),
+        )
+        assert check_mode is False
+
+    def test_all_silence_parameters(self, mocker):
+        sync_mock = mocker.patch.object(utils, 'sync')
+        sync_mock.return_value = True, {}
+        set_module_args(
+            subscription='entity:test-entity',
+            check='check',
+            state='absent',
+            begin=1542671205,
+            expire=1542771205,
+            expire_on_resolve=True,
+            reason='because',
+            labels={'region': 'us-west-1'},
+            annotations={'playbook': 12345},
+        )
+
+        with pytest.raises(AnsibleExitJson):
+            silence.main()
+
+        state, _client, path, payload, check_mode = sync_mock.call_args[0]
+        assert state == 'absent'
+        assert path == '/silenced/entity:test-entity:check'
+        assert payload == dict(
+            subscription='entity:test-entity',
+            check='check',
+            begin=1542671205,
+            expire=1542771205,
+            expire_on_resolve=True,
+            reason='because',
+            metadata=dict(
+                name='entity:test-entity:check',
+                namespace='default',
+                labels={'region': 'us-west-1'},
+                annotations={'playbook': '12345'},
+            ),
+        )
+        assert check_mode is False
+
+    def test_failure_when_both_params_are_missing(self):
+        set_module_args()
+
+        with pytest.raises(AnsibleFailJson,
+                           match='one of the following is required: subscription, check'):
+            silence.main()
+
+    def test_failure(self, mocker):
+        sync_mock = mocker.patch.object(utils, 'sync')
+        sync_mock.side_effect = errors.Error('Bad error')
+        set_module_args(
+            subscription='subscription'
+        )
+
+        with pytest.raises(AnsibleFailJson):
+            silence.main()
---
+++ tests/unit/modules/test_silence_info.py
@@ -0,0 +1,47 @@
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+import pytest
+
+from ansible_collections.sensu.sensu_go.plugins.module_utils import (
+    errors, utils,
+)
+from ansible_collections.sensu.sensu_go.plugins.modules import silence_info
+
+from .common.utils import (
+    AnsibleExitJson, AnsibleFailJson, ModuleTestCase, set_module_args,
+)
+
+
+class TestSilenceInfo(ModuleTestCase):
+    def test_get_all_silences(self, mocker):
+        get_mock = mocker.patch.object(utils, "get")
+        get_mock.return_value = [1, 2, 3]
+        set_module_args()
+
+        with pytest.raises(AnsibleExitJson) as context:
+            silence_info.main()
+
+        _client, path = get_mock.call_args[0]
+        assert path == "/silenced"
+        assert context.value.args[0]["objects"] == [1, 2, 3]
+
+    def test_get_single_silence(self, mocker):
+        get_mock = mocker.patch.object(utils, "get")
+        get_mock.return_value = 4
+        set_module_args(subscription="subscription")
+
+        with pytest.raises(AnsibleExitJson) as context:
+            silence_info.main()
+
+        _client, path = get_mock.call_args[0]
+        assert path == "/silenced/subscription:*"
+        assert context.value.args[0]["objects"] == [4]
+
+    def test_failure(self, mocker):
+        get_mock = mocker.patch.object(utils, "get")
+        get_mock.side_effect = errors.Error("Bad error")
+        set_module_args(check="check")
+
+        with pytest.raises(AnsibleFailJson):
+            silence_info.main()
