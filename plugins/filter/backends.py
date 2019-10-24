627b18386d88fecf8029c143aec4a8550aef04be
Tadej Borovšak <tadej.borovsak@xlab.si>
2019-10-24 21:44:27 +0200

    Add a filter for generating back-end URLs
    This filter can generate a list of backend URLs that can
    be used in Sensu agent configuration files.

---
+++ plugins/filter/backends.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2019, XLAB Steampunk <steampunk@xlab.si>
+#
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+
+def _format_backend(vars):
+    return "{0}://{1}:{2}".format("ws", vars["inventory_hostname"], 8081)
+
+
+def backends(hostvars, host_names):
+    return [_format_backend(hostvars[name]) for name in host_names]
+
+
+class FilterModule(object):
+    def filters(self):
+        return dict(
+            backends=backends,
+        )
