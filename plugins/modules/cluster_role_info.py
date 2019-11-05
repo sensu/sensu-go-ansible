8d6b9f6d931517f930e626bf356323d577b1bb66
Aljaž Košir <aljazkosir5@gmail.com>
2019-11-05 22:59:25 +0100

    Update cluster_role and cluster_role_info modules

---
+++ plugins/modules/cluster_role_info.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2019, Paul Arthur <paul.arthur@flowerysong.com>
+# Copyright: (c) 2019, XLAB Steampunk <steampunk@xlab.si>
+#
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'XLAB Steampunk'}
+
+DOCUMENTATION = '''
+module: cluster_role_info
+author:
+  - Paul Arthur (@flowerysong)
+  - Manca Bizjak (@mancabizjak)
+  - Aljaz Kosir (@aljazkosir)
+  - Tadej Borovsak (@tadeboro)
+short_description: Lists Sensu cluster roles
+description:
+  - For more information, refer to the Sensu documentation at
+    U(https://docs.sensu.io/sensu-go/latest/reference/rbac/)
+notes:
+  - Parameter C(auth.namespace) is ignored in this module.
+extends_documentation_fragment:
+  - sensu.sensu_go.base
+  - sensu.sensu_go.info
+'''
+
+EXAMPLES = '''
+- name: List all Sensu cluster roles
+  role_info:
+  register: result
+'''
+
+RETURN = '''
+roles:
+  description: list of Sensu cluster roles
+  returned: always
+  type: list
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible_collections.sensu.sensu_go.plugins.module_utils import (
+    arguments, errors, utils,
+)
+
+
+def main():
+    module = AnsibleModule(
+        supports_check_mode=True,
+        argument_spec=dict(
+            arguments.get_spec("auth"),
+            name=dict()
+        ),
+    )
+
+    module.params['auth']['namespace'] = None  # Making sure we are not fallbacking to default
+    client = arguments.get_sensu_client(module.params["auth"])
+    if module.params["name"]:
+        path = "/clusterroles/{0}".format(module.params["name"])
+    else:
+        path = "/clusterroles"
+
+    try:
+        cluster_roles = utils.get(client, path)
+    except errors.Error as e:
+        module.fail_json(msg=str(e))
+
+    if module.params["name"]:
+        cluster_roles = [cluster_roles]
+    module.exit_json(changed=False, objects=cluster_roles)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/unit/modules/test_cluster_role.py
@@ -0,0 +1,122 @@
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+import pytest
+
+from ansible_collections.sensu.sensu_go.plugins.module_utils import (
+    errors, utils
+)
+from ansible_collections.sensu.sensu_go.plugins.modules import cluster_role
+
+from .common.utils import (
+    AnsibleExitJson, AnsibleFailJson, ModuleTestCase, set_module_args,
+)
+
+
+class TestClusterRole(ModuleTestCase):
+    def test_minimal_cluster_role_parameters(self, mocker):
+        sync_mock = mocker.patch.object(utils, 'sync')
+        sync_mock.return_value = True, {}
+        set_module_args(
+            name='test_cluster_role',
+            rules=[
+                dict(
+                    verbs=[],
+                    resources=[]
+                ),
+            ]
+        )
+
+        with pytest.raises(AnsibleExitJson):
+            cluster_role.main()
+
+        state, _client, path, payload, check_mode, _compare = sync_mock.call_args[0]
+        assert state == 'present'
+        assert path == '/clusterroles/test_cluster_role'
+        assert payload == dict(
+            rules=[
+                dict(
+                    verbs=[],
+                    resources=[],
+                    resource_names=None,
+                )
+            ],
+            metadata=dict(
+                name='test_cluster_role',
+            ),
+        )
+        assert check_mode is False
+
+    def test_all_cluster_role_parameters(self, mocker):
+        sync_mock = mocker.patch.object(utils, 'sync')
+        sync_mock.return_value = True, {}
+        set_module_args(
+            name='test_cluster_role',
+            state='present',
+            rules=[
+                dict(
+                    verbs=['get', 'list', 'create'],
+                    resources=['assets', 'entities'],
+                ),
+                dict(
+                    verbs=['list'],
+                    resources=['check'],
+                    resource_names=['my-check'],
+                )
+            ],
+        )
+
+        with pytest.raises(AnsibleExitJson):
+            cluster_role.main()
+
+        state, _client, path, payload, check_mode, _compare = sync_mock.call_args[0]
+        assert state == 'present'
+        assert path == '/clusterroles/test_cluster_role'
+        assert payload == dict(
+            metadata=dict(
+                name='test_cluster_role',
+            ),
+            rules=[
+                dict(
+                    verbs=['get', 'list', 'create'],
+                    resources=['assets', 'entities'],
+                    resource_names=None,
+                ),
+                dict(
+                    verbs=['list'],
+                    resources=['check'],
+                    resource_names=['my-check'],
+                )
+            ],
+        )
+
+    def test_failure(self, mocker):
+        sync_mock = mocker.patch.object(utils, 'sync')
+        sync_mock.side_effect = errors.Error("Bad error")
+        set_module_args(
+            name='test_cluster_role',
+            rules=[
+                dict(
+                    verbs=[],
+                    resources=[],
+                )
+            ],
+        )
+        with pytest.raises(AnsibleFailJson):
+            cluster_role.main()
+
+    def test_failure_invalid_verb(self, mocker):
+        sync_mock = mocker.patch.object(utils, 'sync')
+        sync_mock.side_effect = Exception("Validation should fail but didn't")
+        set_module_args(
+            name='test_cluster_role',
+            rules=[
+                dict(
+                    verbs=['list', 'invalid'],
+                    resources=[],
+                ),
+            ]
+        )
+
+        with pytest.raises(AnsibleFailJson):
+            cluster_role.main()
