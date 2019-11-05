400e1197fcc5cd088de4d560d5d53ac2f2b3bcc4
Manca Bizjak <manca.bizjak@xlab.si>
2019-11-05 15:30:55 +0100

    Update role_binding and role_binding_info modules
    This commit updates two modules: role_binding and role_binding_info.
    Following the scoping of recently updated role and role_info modules,
    role binding modules also deal with namespaced roles only, meaning
    that the cluster role bindings were omitted.
    
    Signed-off-by: Manca Bizjak <manca.bizjak@xlab.si>

---
+++ plugins/modules/role_binding.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2019, Paul Arthur <paul.arthur@flowerysong.com>
+# Copyright: (c) 2019, XLAB Steampunk <steampunk@xlab.si>
+#
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'XLAB Steampunk'}
+
+DOCUMENTATION = '''
+module: role_binding
+author:
+ - Paul Arthur (@flowerysong)
+ - Manca Bizjak (@mancabizjak)
+ - Aljaz Kosir (@aljazkosir)
+ - Tadej Borovsak (@tadeboro)
+short_description: Manages Sensu role bindings
+description:
+  - For more information, refer to the Sensu documentation at
+    U(https://docs.sensu.io/sensu-go/latest/reference/rbac/)
+extends_documentation_fragment:
+  - sensu.sensu_go.auth
+  - sensu.sensu_go.name
+  - sensu.sensu_go.state
+options:
+  role:
+    description:
+      - Name of the role
+    type: str
+  users:
+    description:
+      - List of users to bind to the role
+      - Note that at least one of 'users' and 'groups' must be
+        specified when creating a role binding.
+    type: list
+  groups:
+    description:
+      - List of groups to bind to the role
+      - Note that at least one of 'users' and 'groups' must be
+        specified when creating a role binding.
+    type: list
+'''
+
+EXAMPLES = '''
+- name: Create a role binding
+  role_binding:
+    name: dev_and_testing
+    role: testers_permissive
+    groups:
+        - testers
+        - dev
+        - ops
+    users:
+      - alice
+'''
+
+RETURN = '''
+object:
+    description: object representing Sensu role binding
+    returned: success
+    type: dict
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible_collections.sensu.sensu_go.plugins.module_utils import (
+    arguments, errors, utils, role_utils
+)
+
+
+def validate_module_params(module):
+    params = module.params
+    if params["state"] == "present":
+        if not (params["users"] or params["groups"]):
+            module.fail_json(
+                msg="missing required parameters: users or groups"
+            )
+
+
+def build_api_payload(params):
+    payload = arguments.get_mutation_payload(params)
+    payload["subjects"] = role_utils.build_subjects(params["groups"], params["users"])
+    payload["role_ref"] = role_utils.type_name_dict("Role", params["role"])
+
+    return payload
+
+
+def main():
+    required_if = [
+        ("state", "present", ["role"])
+    ]
+    module = AnsibleModule(
+        required_if=required_if,
+        supports_check_mode=True,
+        argument_spec=dict(
+            arguments.get_spec("auth", "name", "state"),
+            role=dict(),
+            users=dict(
+                type="list",
+            ),
+            groups=dict(
+                type="list",
+            ),
+        )
+    )
+
+    validate_module_params(module)
+    client = arguments.get_sensu_client(module.params["auth"])
+    path = "/rolebindings/{0}".format(module.params["name"])
+    payload = build_api_payload(module.params)
+
+    try:
+        changed, role_binding = utils.sync(
+            module.params["state"], client, path, payload, module.check_mode, role_utils.do_role_bindings_differ
+        )
+        module.exit_json(changed=changed, object=role_binding)
+    except errors.Error as e:
+        module.fail_json(msg=str(e))
+
+
+if __name__ == '__main__':
+    main()
---
+++ plugins/modules/role_binding_info.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2019, Paul Arthur <paul.arthur@flowerysong.com>
+# Copyright: (c) 2019, XLAB Steampunk <steampunk@xlab.si>
+#
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'XLAB Steampunk'}
+
+DOCUMENTATION = '''
+module: role_binding_info
+author:
+  - Paul Arthur (@flowerysong)
+  - Manca Bizjak (@mancabizjak)
+  - Aljaz Kosir (@aljazkosir)
+  - Tadej Borovsak (@tadeboro)
+short_description: Lists Sensu role bindings
+description:
+  - For more information, refer to the Sensu documentation at
+    U(https://docs.sensu.io/sensu-go/latest/reference/rbac/)
+version_added: 0.1.0
+extends_documentation_fragment:
+  - sensu.sensu_go.base
+  - sensu.sensu_go.info
+'''
+
+EXAMPLES = '''
+- name: List all Sensu role bindings
+  role_binding_info:
+  register: result
+'''
+
+RETURN = '''
+role_bindings:
+  description: list of Sensu role bindings
+  returned: always
+  type: list
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible_collections.sensu.sensu_go.plugins.module_utils import (
+    arguments, errors, utils,
+)
+
+
+def main():
+    module = AnsibleModule(
+        supports_check_mode=True,
+        argument_spec=dict(
+            arguments.get_spec("auth"),
+            name=dict()
+        )
+    )
+
+    client = arguments.get_sensu_client(module.params["auth"])
+    if module.params["name"]:
+        path = "/rolebindings/{0}".format(module.params["name"])
+    else:
+        path = "/rolebindings"
+
+    try:
+        role_bindings = utils.get(client, path)
+    except errors.Error as e:
+        module.fail_json(msg=str(e))
+
+    if module.params["name"]:
+        role_bindings = [role_bindings]
+    module.exit_json(changed=False, objects=role_bindings)
+
+
+if __name__ == '__main__':
+    main()
