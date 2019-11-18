f4028b96e340564cd9036766264c1dc121379467
Aljaž Košir <aljazkosir5@gmail.com>
2019-11-18 11:34:45 +0100

    Update event_info module
    With this commit, we update event_info module to the new structure.
    API supports filtering by entity and by entity and check together,
    but does not support filtering by check, if this is needed we can filter
    this locally. That's why I created a separate PR for this,
    so we can discuss this here. Module is currently not backed with
    extensive integration test suite, due to the lack of event module.

---
+++ tests/unit/modules/test_event_info.py
@@ -0,0 +1,75 @@
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+import pytest
+
+from ansible_collections.sensu.sensu_go.plugins.module_utils import (
+    errors, utils,
+)
+from ansible_collections.sensu.sensu_go.plugins.modules import event_info
+
+from .common.utils import (
+    AnsibleExitJson, AnsibleFailJson, ModuleTestCase, set_module_args,
+)
+
+
+class TestEventInfo(ModuleTestCase):
+    def test_get_all_events(self, mocker):
+        get_mock = mocker.patch.object(utils, 'get')
+        get_mock.return_value = [1, 2, 3]
+        set_module_args()
+
+        with pytest.raises(AnsibleExitJson) as context:
+            event_info.main()
+
+        _client, path = get_mock.call_args[0]
+        assert path == '/events'
+        assert context.value.args[0]['objects'] == [1, 2, 3]
+
+    def test_get_events_by_entity(self, mocker):
+        get_mock = mocker.patch.object(utils, 'get')
+        get_mock.return_value = [1, 2]
+        set_module_args(
+            entity='simple-entity'
+        )
+
+        with pytest.raises(AnsibleExitJson) as context:
+            event_info.main()
+
+        _client, path = get_mock.call_args[0]
+        assert path == '/events/simple-entity'
+        assert context.value.args[0]['objects'] == [1, 2]
+
+    def test_get_events_by_check(self, mocker):
+        get_mock = mocker.patch.object(utils, 'get')
+        get_mock.return_value = [1, 2]
+        set_module_args(
+            check='simple-check'
+        )
+
+        with pytest.raises(AnsibleFailJson,
+                           match=r"missing parameter\(s\) required by 'check': entity"):
+            event_info.main()
+
+    def test_get_single_event_by_entity_and_check(self, mocker):
+        get_mock = mocker.patch.object(utils, 'get')
+        get_mock.return_value = 4
+        set_module_args(
+            entity='simple-entity',
+            check='simple-check'
+        )
+
+        with pytest.raises(AnsibleExitJson) as context:
+            event_info.main()
+
+        _client, path = get_mock.call_args[0]
+        assert path == '/events/simple-entity/simple-check'
+        assert context.value.args[0]['objects'] == [4]
+
+    def test_failure(self, mocker):
+        get_mock = mocker.patch.object(utils, 'get')
+        get_mock.side_effect = errors.Error('Bad error')
+        set_module_args(entity='simple-entity')
+
+        with pytest.raises(AnsibleFailJson):
+            event_info.main()
