9e0812229031a95daae6d646a07bfc3e396b8da0
Aljaž Košir <aljazkosir5@gmail.com>
2019-11-18 11:34:45 +0100

    Update event_info module
    With this commit, we update event_info module to the new structure.
    API supports filtering by entity and by entity and check together,
    but does not support filtering by check, if this is needed we can filter
    this locally. That's why I created a separate PR for this,
    so we can discuss this here. Module is currently not backed with
    extensive integration test suite, due to the lack of event module.

---
+++ plugins/modules/event_info.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2019, Paul Arthur <paul.arthur@flowerysong.com>
+# Copyright: (c) 2019, XLAB Steampunk <steampunk@xlab.si>
+#
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'XLAB Steampunk'}
+
+DOCUMENTATION = '''
+module: event_info
+author:
+  - Paul Arthur (@flowerysong)
+  - Aljaz Kosir (@aljazkosir)
+  - Manca Bizjak (@mancabizjak)
+  - Tadej Borovsak (@tadeboro)
+short_description: Lists Sensu events
+description:
+  - For more information, refer to the Sensu documentation at
+    U(https://docs.sensu.io/sensu-go/latest/reference/events/)
+extends_documentation_fragment:
+  - sensu.sensu_go.base
+options:
+  check:
+    description:
+      - Limit results to a specific check.
+      - C(entity) must also be specified.
+    type: str
+  entity:
+    description:
+      - Limit results to a specific entity.
+    type: str
+'''
+
+EXAMPLES = '''
+- name: List Sensu events
+  event_info:
+  register: result
+
+- name: List Sensu events for api.example.com
+  event_info:
+    entity: api.example.com
+  register: result
+
+- name: Filter events by check and entity
+  event_info:
+    entity: api.example.com
+    check: check-cpu
+  register: result
+'''
+
+RETURN = '''
+objects:
+  description: list of Sensu events
+  returned: always
+  type: list
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible_collections.sensu.sensu_go.plugins.module_utils import (
+    arguments, errors, utils,
+)
+
+
+def main():
+    required_by = {'check': ['entity']}
+    module = AnsibleModule(
+        supports_check_mode=True,
+        required_by=required_by,
+        argument_spec=dict(
+            arguments.get_spec("auth"),
+            check=dict(),
+            entity=dict(),
+        ),
+    )
+
+    client = arguments.get_sensu_client(module.params['auth'])
+    path_components = '/events', module.params['entity'], module.params['check']
+    path = '/'.join(c for c in path_components if c)
+
+    try:
+        events = utils.get(client, path)
+    except errors.Error as e:
+        module.fail_json(msg=str(e))
+
+    if module.params['check']:
+        events = [events]
+    module.exit_json(changed=False, objects=events)
+
+
+if __name__ == '__main__':
+    main()
