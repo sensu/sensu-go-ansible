6179ba22e9dee3a26b621347830f0ad3eb8bd7ac
Tadej Borovšak <tadej.borovsak@xlab.si>
2019-12-06 12:57:49 +0100

    Add links to GitHub source files
    Because our documentation is not perfect by any means, we added source
    links to documentation pages. This should make it easier for people
    who find an error in our docs to prepare an error report and maybe
    even submit a fix.
    
    We took special care to link auto-generated documentation to the
    relevant sources. Currently, this means that module reference
    documentation points directly to the module source code where the
    documentation lives.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,3 +8,10 @@ extensions = [
 exclude_patterns = []
 
 html_theme = "sphinx_rtd_theme"
+html_context = {
+    "display_github": True,
+    "github_user": "sensu",
+    "github_repo": "sensu-go-ansible",
+    "github_version": "master",
+    "conf_py_path": "/docs/source/",
+}
