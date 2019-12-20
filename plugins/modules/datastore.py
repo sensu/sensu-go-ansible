c6f6e3448f5cbab45436c35df0629ef191d8bb7b
Tadej Borovšak <tadej.borovsak@xlab.si>
2019-12-20 11:02:12 +0100

    Add external datastore modules
    Datastore module and its info counterpart know how to deal with
    external datastores.
    
    Payload that the Sensu storage API accepts is a bit different from the
    payload that core/v2 API endpoints expect, but we kept the user-facing
    interface similar to all other modules. This way, playbook authors can
    use their knowledge about existing modules when dealing with data
    storages.
    
    Another peculiarity with datastore module is its reluctance to break
    the backend. This means that datastore module will abort adding
    external datastore if one is already found on the backend. Note that
    it is still possible to remove and update datastores even if there is
    more than one on the backend.

---
+++ plugins/modules/datastore.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2019, XLAB Steampunk <steampunk@xlab.si>
+#
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+ANSIBLE_METADATA = {
+    "metadata_version": "1.1",
+    "status": ["stableinterface"],
+    "supported_by": "certified",
+}
+
+DOCUMENTATION = '''
+module: datastore
+author:
+  - Manca Bizjak (@mancabizjak)
+  - Tadej Borovsak (@tadeboro)
+short_description: Manage Sensu external datastore providers
+description:
+  - Add or remove external datastore provider.
+  - For more information, refer to the Sensu documentation at
+    U(https://docs.sensu.io/sensu-go/latest/reference/datastore/).
+version_added: "1.1"
+extends_documentation_fragment:
+  - sensu.sensu_go.auth
+  - sensu.sensu_go.name
+  - sensu.sensu_go.state
+seealso:
+  - module: datastore_info
+options:
+  dsn:
+    description:
+      - Attribute that specifies the data source names as a URL or
+        PostgreSQL connection string. See the PostgreSQL docs for more
+        information about connection strings.
+    type: str
+  pool_size:
+    description:
+      - The maximum number of connections to hold in the PostgreSQL connection
+        pool.
+    type: int
+notes:
+  - Currently, only one external datastore can be active at a time. The module
+    will fail to perform its operation if this would break that invariant.
+'''
+
+EXAMPLES = '''
+- name: Add external datastore
+  datastore:
+    name: my-postgres
+    dsn: postgresql://user:secret@host:port/dbname
+
+- name: Remove external datastore
+  datastore:
+    name: my-postgres
+    state: absent
+'''
+
+RETURN = '''
+object:
+    description: object representing external datastore provider
+    returned: success
+    type: dict
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+
+from ansible_collections.sensu.sensu_go.plugins.module_utils import (
+    arguments, errors, utils,
+)
+
+API_GROUP = "enterprise"
+API_VERSION = "store/v1"
+
+
+def _do_differ(remote, payload):
+    return utils.do_differ(remote["spec"], payload["spec"])
+
+
+def sync(state, client, list_path, resource_path, payload, check_mode):
+    datastore = utils.get(client, resource_path)
+
+    # When we are deleting stores, we do not care if there is more than one
+    # datastore present. We just make sure the currently manipulated store is
+    # gone. This makes our module useful in "let us clean up the mess"
+    # scenarios.
+    if state == "absent" and datastore is None:
+        return False, None
+
+    if state == "absent":
+        if not check_mode:
+            utils.delete(client, resource_path)
+        return True, None
+
+    # If the store exists, update it and ignore the fact that there might be
+    # more than one present.
+    if datastore:
+        if _do_differ(datastore, payload):
+            if check_mode:
+                return True, payload
+            utils.put(client, resource_path, payload)
+            return True, utils.get(client, resource_path)
+        return False, datastore
+
+    # When adding a new datastore, we first make sure there is no other
+    # datastore present because we do not want to be the ones who brought
+    # backends into an inconsistent state.
+    if utils.get(client, list_path):
+        raise errors.Error("Some other external datastore is already active.")
+
+    if check_mode:
+        return True, payload
+    utils.put(client, resource_path, payload)
+    return True, utils.get(client, resource_path)
+
+
+def main():
+    required_if = [
+        ("state", "present", ["dsn"])
+    ]
+    module = AnsibleModule(
+        required_if=required_if,
+        supports_check_mode=True,
+        argument_spec=dict(
+            arguments.get_spec("auth", "name", "state"),
+            dsn=dict(),
+            pool_size=dict(
+                type="int",
+            )
+        ),
+    )
+
+    client = arguments.get_sensu_client(module.params["auth"])
+    list_path = utils.build_url_path(API_GROUP, API_VERSION, None, "provider")
+    resource_path = utils.build_url_path(
+        API_GROUP, API_VERSION, None, "provider", module.params["name"],
+    )
+    payload = dict(
+        type="PostgresConfig",
+        api_version=API_VERSION,
+        spec=arguments.get_mutation_payload(module.params, "dsn", "pool_size"),
+    )
+
+    try:
+        changed, datastore = sync(
+            module.params["state"], client, list_path, resource_path, payload,
+            module.check_mode,
+        )
+        # We simulate the behavior of v2 API here and only return the spec.
+        module.exit_json(
+            changed=changed, object=datastore and datastore["spec"],
+        )
+    except errors.Error as e:
+        module.fail_json(msg=str(e))
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/unit/modules/test_datastore.py
@@ -0,0 +1,272 @@
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+import pytest
+
+from ansible_collections.sensu.sensu_go.plugins.module_utils import (
+    errors, http, utils,
+)
+from ansible_collections.sensu.sensu_go.plugins.modules import datastore
+
+from .common.utils import (
+    AnsibleExitJson, AnsibleFailJson, ModuleTestCase, set_module_args,
+)
+
+
+class TestSync(ModuleTestCase):
+    def test_absent_no_current_object(self, mocker):
+        client = mocker.Mock()
+        client.get.return_value = http.Response(404, "")
+
+        changed, object = datastore.sync(
+            "absent", client, "/list", "/resource", {}, False,
+        )
+
+        assert changed is False
+        assert object is None
+
+    def test_absent_no_current_object_check(self, mocker):
+        client = mocker.Mock()
+        client.get.return_value = http.Response(404, "")
+
+        changed, object = datastore.sync(
+            "absent", client, "/list", "/resource", {}, True,
+        )
+
+        assert changed is False
+        assert object is None
+
+    def test_absent_current_object_present(self, mocker):
+        client = mocker.Mock()
+        client.get.return_value = http.Response(200, '{}')
+        client.delete.return_value = http.Response(204, "")
+
+        changed, object = datastore.sync(
+            "absent", client, "/list", "/resource", {}, False,
+        )
+
+        assert changed is True
+        assert object is None
+        client.delete.assert_called_with("/resource")
+
+    def test_absent_current_object_present_check(self, mocker):
+        client = mocker.Mock()
+        client.get.return_value = http.Response(200, '{}')
+        client.delete.return_value = http.Response(204, "")
+
+        changed, object = datastore.sync(
+            "absent", client, "/list", "/resource", {}, True,
+        )
+
+        assert changed is True
+        assert object is None
+        client.delete.assert_not_called()
+
+    def test_present_current_object_differ(self, mocker):
+        client = mocker.Mock()
+        client.get.side_effect = (
+            http.Response(200, '{"spec": {"current": "data"}}'),
+            http.Response(200, '{"spec": {"new": "data"}}'),
+        )
+        client.put.return_value = http.Response(201, "")
+
+        changed, object = datastore.sync(
+            "present", client, "/list", "/resource", {"spec": {"my": "data"}},
+            False,
+        )
+
+        assert changed is True
+        assert {"spec": {"new": "data"}} == object
+        client.put.assert_called_once_with(
+            "/resource", {"spec": {"my": "data"}},
+        )
+
+    def test_present_current_object_differ_check(self, mocker):
+        client = mocker.Mock()
+        client.get.return_value = (
+            http.Response(200, '{"spec": {"current": "data"}}')
+        )
+
+        changed, object = datastore.sync(
+            "present", client, "/list", "/resource", {"spec": {"my": "data"}},
+            True,
+        )
+
+        assert changed is True
+        assert {"spec": {"my": "data"}} == object
+        client.put.assert_not_called()
+
+    def test_present_current_object_does_not_differ(self, mocker):
+        client = mocker.Mock()
+        client.get.return_value = (
+            http.Response(200, '{"spec": {"my": "data"}}')
+        )
+
+        changed, object = datastore.sync(
+            "present", client, "/list", "/resource", {"spec": {"my": "data"}},
+            False,
+        )
+
+        assert changed is False
+        assert {"spec": {"my": "data"}} == object
+        client.put.assert_not_called()
+
+    def test_present_current_object_does_not_differ_check(self, mocker):
+        client = mocker.Mock()
+        client.get.return_value = (
+            http.Response(200, '{"spec": {"my": "data"}}')
+        )
+
+        changed, object = datastore.sync(
+            "present", client, "/list", "/resource", {"spec": {"my": "data"}},
+            True,
+        )
+
+        assert changed is False
+        assert {"spec": {"my": "data"}} == object
+        client.put.assert_not_called()
+
+    def test_present_no_current_object_empty_backend(self, mocker):
+        client = mocker.Mock()
+        client.get.side_effect = (
+            http.Response(404, ""),
+            http.Response(200, "[]"),
+            http.Response(200, '{"spec": {"new": "data"}}'),
+        )
+        client.put.return_value = http.Response(201, "")
+
+        changed, object = datastore.sync(
+            "present", client, "/list", "/resource", {"spec": {"my": "data"}},
+            False,
+        )
+
+        assert changed is True
+        assert {"spec": {"new": "data"}} == object
+        client.put.assert_called_once_with(
+            "/resource", {"spec": {"my": "data"}},
+        )
+
+    def test_present_no_current_object_empty_backend_check(self, mocker):
+        client = mocker.Mock()
+        client.get.side_effect = (
+            http.Response(404, ""),
+            http.Response(200, "[]"),
+        )
+
+        changed, object = datastore.sync(
+            "present", client, "/list", "/resource", {"spec": {"my": "data"}},
+            True,
+        )
+
+        assert changed is True
+        assert {"spec": {"my": "data"}} == object
+        client.put.assert_not_called()
+
+    @pytest.mark.parametrize("check", [False, True])
+    def test_present_no_current_object_non_empty_backend(self, mocker, check):
+        client = mocker.Mock()
+        client.get.side_effect = (
+            http.Response(404, ""),
+            http.Response(200, "[{}]"),
+        )
+
+        with pytest.raises(errors.Error, match="already active"):
+            datastore.sync(
+                "present", client, "/list", "/resource",
+                {"spec": {"my": "data"}}, check,
+            )
+
+        client.put.assert_not_called()
+
+
+class TestDatastore(ModuleTestCase):
+    def test_minimal_datastore_parameters_present(self, mocker):
+        sync_mock = mocker.patch.object(datastore, "sync")
+        sync_mock.return_value = True, {}
+        set_module_args(
+            name="test_datastore",
+            dsn="my-dsn",
+        )
+
+        with pytest.raises(AnsibleExitJson):
+            datastore.main()
+
+        state, _client, list_path, resource_path, payload, check_mode = (
+            sync_mock.call_args[0]
+        )
+        assert state == "present"
+        assert resource_path == "/api/enterprise/store/v1/provider/test_datastore"
+        assert list_path == "/api/enterprise/store/v1/provider"
+        assert payload == dict(
+            type="PostgresConfig",
+            api_version="store/v1",
+            spec=dict(
+                metadata=dict(
+                    name="test_datastore",
+                ),
+                dsn="my-dsn",
+            ),
+        )
+        assert check_mode is False
+
+    def test_minimal_datastore_parameters_absent(self, mocker):
+        sync_mock = mocker.patch.object(datastore, "sync")
+        sync_mock.return_value = True, {}
+        set_module_args(
+            name="test_datastore",
+            state="absent",
+        )
+
+        with pytest.raises(AnsibleExitJson):
+            datastore.main()
+
+        state, _client, list_path, resource_path, _payload, check_mode = (
+            sync_mock.call_args[0]
+        )
+        assert state == "absent"
+        assert resource_path == "/api/enterprise/store/v1/provider/test_datastore"
+        assert list_path == "/api/enterprise/store/v1/provider"
+        assert check_mode is False
+
+    def test_all_datastore_parameters(self, mocker):
+        sync_mock = mocker.patch.object(datastore, "sync")
+        sync_mock.return_value = True, {}
+        set_module_args(
+            name="test_datastore",
+            dsn="my-dsn",
+            pool_size=543,
+        )
+
+        with pytest.raises(AnsibleExitJson):
+            datastore.main()
+
+        state, _client, list_path, resource_path, payload, check_mode = (
+            sync_mock.call_args[0]
+        )
+        assert state == "present"
+        assert resource_path == "/api/enterprise/store/v1/provider/test_datastore"
+        assert list_path == "/api/enterprise/store/v1/provider"
+        assert payload == dict(
+            type="PostgresConfig",
+            api_version="store/v1",
+            spec=dict(
+                metadata=dict(
+                    name="test_datastore",
+                ),
+                dsn="my-dsn",
+                pool_size=543,
+            ),
+        )
+        assert check_mode is False
+
+    def test_failure(self, mocker):
+        sync_mock = mocker.patch.object(utils, "sync")
+        sync_mock.side_effect = errors.Error("Bad error")
+        set_module_args(
+            name="test_datastore",
+            dsn="my-dsn",
+        )
+
+        with pytest.raises(AnsibleFailJson):
+            datastore.main()
