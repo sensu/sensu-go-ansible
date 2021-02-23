ffee6220d3a1359f347e569681a71763c2aaf6cf
Tadej Borovšak <tadej.borovsak@xlab.si>
2021-02-23 16:47:36 +0100

    Add modules for managing clusters
    This is the second part of federation support in Sensu Go Ansible
    collection.

---
+++ tests/unit/modules/test_cluster.py
@@ -0,0 +1,77 @@
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+import pytest
+
+from ansible_collections.sensu.sensu_go.plugins.module_utils import (
+    errors, utils,
+)
+from ansible_collections.sensu.sensu_go.plugins.modules import cluster
+
+from .common.utils import (
+    AnsibleExitJson, AnsibleFailJson, ModuleTestCase, set_module_args,
+)
+
+
+class TestCluster(ModuleTestCase):
+    @pytest.mark.parametrize("params", [
+        {"name": "demo", "state": "absent"},
+        {"name": "demo", "api_urls": "url"},
+    ])
+    def test_minimal_parameters(self, mocker, params):
+        mocker.patch.object(utils, "sync_v1").return_value = True, {}
+        set_module_args(**params)
+
+        with pytest.raises(AnsibleExitJson):
+            cluster.main()
+
+    def test_all_parameters(self, mocker):
+        sync_mock = mocker.patch.object(utils, "sync_v1")
+        sync_mock.return_value = True, {}
+        set_module_args(
+            auth=dict(
+                user="user",
+                password="pass",
+                url="http://127.0.0.1:1234",
+                api_key="123-key",
+                verify=False,
+                ca_path="/tmp/ca.bundle",
+            ),
+            state="present",
+            name="demo",
+            api_urls=["a", "b"],
+        )
+
+        with pytest.raises(AnsibleExitJson):
+            cluster.main()
+
+        state, _client, path, payload, check_mode = sync_mock.call_args[0]
+        assert state == "present"
+        assert path == "/api/enterprise/federation/v1/clusters/demo"
+        assert payload == dict(
+            type="Cluster",
+            api_version="federation/v1",
+            metadata=dict(name="demo"),
+            spec=dict(api_urls=["a", "b"]),
+        )
+        assert check_mode is False
+
+    @pytest.mark.parametrize("skip", ["api_urls"])
+    def test_missing_required_param_present(self, mocker, skip):
+        sync_mock = mocker.patch.object(utils, "sync_v1")
+        all_args = dict(name="demo", api_urls="url")
+        set_module_args(**{k: v for k, v in all_args.items() if k != skip})
+
+        with pytest.raises(AnsibleFailJson):
+            cluster.main()
+
+        sync_mock.assert_not_called()
+
+    def test_failure(self, mocker):
+        mocker.patch.object(utils, "sync_v1").side_effect = (
+            errors.Error("Bad error")
+        )
+        set_module_args(name="demo", state="absent")
+
+        with pytest.raises(AnsibleFailJson):
+            cluster.main()
---
+++ tests/unit/modules/test_cluster_info.py
@@ -0,0 +1,79 @@
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+import pytest
+
+from ansible_collections.sensu.sensu_go.plugins.module_utils import (
+    errors, utils,
+)
+from ansible_collections.sensu.sensu_go.plugins.modules import cluster_info
+
+from .common.utils import (
+    AnsibleExitJson, AnsibleFailJson, ModuleTestCase, set_module_args,
+)
+
+
+class TestClusterInfo(ModuleTestCase):
+    def test_all_parameters(self, mocker):
+        get_mock = mocker.patch.object(utils, "get")
+        get_mock.return_value = {"spec": {"k1": "v1"}}
+        set_module_args(
+            auth=dict(
+                user="user",
+                password="pass",
+                url="http://127.0.0.1:1234",
+                api_key="123-key",
+                verify=False,
+                ca_path="/tmp/ca.bundle",
+            ),
+            name="demo",
+        )
+
+        with pytest.raises(AnsibleExitJson):
+            cluster_info.main()
+
+    def test_get_all_clusters(self, mocker):
+        get_mock = mocker.patch.object(utils, "get")
+        get_mock.return_value = [
+            {"spec": {"k1": "v1"}}, {"spec": {"k2": "v2"}},
+        ]
+        set_module_args()
+
+        with pytest.raises(AnsibleExitJson) as context:
+            cluster_info.main()
+
+        _client, path = get_mock.call_args[0]
+        assert path == "/api/enterprise/federation/v1/clusters"
+        assert context.value.args[0]["objects"] == [
+            {"k1": "v1"}, {"k2": "v2"},
+        ]
+
+    def test_get_single_cluster(self, mocker):
+        get_mock = mocker.patch.object(utils, "get")
+        get_mock.return_value = {"spec": {"k3": "v3"}}
+        set_module_args(name="demo")
+
+        with pytest.raises(AnsibleExitJson) as context:
+            cluster_info.main()
+
+        _client, path = get_mock.call_args[0]
+        assert path == "/api/enterprise/federation/v1/clusters/demo"
+        assert context.value.args[0]["objects"] == [{"k3": "v3"}]
+
+    def test_missing_single_cluster(self, mocker):
+        get_mock = mocker.patch.object(utils, "get")
+        get_mock.return_value = None
+        set_module_args(name="demo")
+
+        with pytest.raises(AnsibleExitJson) as context:
+            cluster_info.main()
+
+        assert context.value.args[0]["objects"] == []
+
+    def test_failure(self, mocker):
+        get_mock = mocker.patch.object(utils, "get")
+        get_mock.side_effect = errors.Error("Bad error")
+        set_module_args(name="demo")
+
+        with pytest.raises(AnsibleFailJson):
+            cluster_info.main()
