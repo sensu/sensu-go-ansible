36729132faf20b2dce977e9824bbb6f611c87d77
Domen <domen.dobnikar@xlab.si>
2023-07-19 09:36:16 +0200

    pipeline_info

--- plugins/modules/pipeline_info.py
+++ plugins/modules/pipeline_info.py
@@ -17,7 +17,7 @@ DOCUMENTATION = '''
 module: pipeline_info
 author:
   - Domen Dobnikar (@domen_dobnikar)
-short_description: List available Sensu pipelines
+short_description: List available Sensu pipelines.
 description:
   - Retrieve information about Sensu Go pipelines.
   - For more information, refer to the Sensu documentation at
@@ -26,12 +26,8 @@ version_added: 1.0.0
 extends_documentation_fragment:
   - sensu.sensu_go.requirements
   - sensu.sensu_go.auth
-  - sensu.sensu_go.name
   - sensu.sensu_go.namespace
-  - sensu.sensu_go.state
-  - sensu.sensu_go.labels
-  - sensu.sensu_go.annotations
-  - sensu.sensu_go.secrets
+
 seealso:
   - module: sensu.sensu_go.socket_handler
   - module: sensu.sensu_go.handler_info
@@ -41,128 +37,82 @@ seealso:
   - module: sensu.sensu_go.mutator
   - module: sensu.sensu_go.mutator_info
 options:
-  command:
+  name:
     description:
-      - The handler command to be executed. The event data is passed to the
-        process through STDIN.
-      - Required if I(state) is C(present).
+      - Name of a specific pipeline.
+      - .
     type: str
-  filters:
-    description:
-      - List of filters to use when determining whether to pass the check result to this handler.
-    type: list
-    elements: str
-  mutator:
-    description:
-      - Mutator to call for transforming the check result before passing it to this handler.
-    type: str
-  timeout:
-    description:
-      - Timeout for handler execution.
-    type: int
-  env_vars:
-    description:
-      - A mapping of environment variable names and values to use with command execution.
-    type: dict
-  runtime_assets:
-    description:
-      - List of runtime assets to required to run the handler C(command).
-    type: list
-    elements: str
 '''
 
 EXAMPLES = '''
-- name: Setup InfluxDB handler
-  sensu.sensu_go.pipe_handler:
-    name: influx-db
-    command: sensu-influxdb-handler -d sensu
-    env_vars:
-      INFLUXDB_ADDR: http://influxdb.default.svc.cluster.local:8086
-      INFLUXDB_USER: sensu
-      INFLUXDB_PASS: password
-    runtime_assets:
-      - sensu-influxdb-handler
+- name: List all Sensu pipelines
+  sensu.sensu_go.pipeline_info:
+  register: result
 
-- name: Delete  handler
-  sensu.sensu_go.pipe_handler:
-    name: influx-db
-    state: absent
+- name: List the selected Sensu pipeline
+  sensu.sensu_go.pipeline_info:
+    name: my_pipeline
+  register: result
 '''
 
-RETURN = '''
-object:
-  description: Object representing Sensu pipe handler.
+RETURN = """
+objects:
+  description: List of Sensu pipelines.
   returned: success
-  type: dict
+  type: list
+  elements: dict
   sample:
-    metadata:
-      name: pipe_handler_minimum
-      namespace: default
-    command: command-example
-    type: pipe
-'''
+    - metadata:
+        created_by: admin
+        name: this_pipeline
+        namespace: default
+      workflows: null
+    - metadata:
+        created_by: admin
+        name: this_pipeline_1
+        namespace: default
+      workflows:
+      - handler:
+          api_version: core/v2
+          name: best_handler_1
+          type: Handler
+        name: best_workflow_1
+    - metadata:
+        created_by: admin
+        name: this_pipeline_2
+        namespace: default
+      workflows:
+      - handler:
+          api_version: core/v2
+          name: best_handler_2
+          type: Handler
+        name: best_workflow_1
+"""
 
 from ansible.module_utils.basic import AnsibleModule
 
 from ..module_utils import arguments, errors, utils
 
 
-def do_differ(current, desired):
-    return (
-        utils.do_differ(current, desired, "secrets") or
-        utils.do_secrets_differ(current, desired)
-    )
-
-
 def main():
-    required_if = [
-        ('state', 'present', ['command'])
-    ]
     module = AnsibleModule(
         supports_check_mode=True,
-        required_if=required_if,
         argument_spec=dict(
-            arguments.get_spec(
-                "auth", "name", "state", "labels", "annotations", "namespace",
-                "secrets",
-            ),
-            command=dict(),
-            filters=dict(
-                type='list', elements='str',
-            ),
-            mutator=dict(),
-            timeout=dict(
-                type='int'
-            ),
-            env_vars=dict(
-                type='dict'
-            ),
-            runtime_assets=dict(
-                type='list', elements='str',
-            ),
+            arguments.get_spec("auth", "namespace"),
+            name=dict(),  # Name is not required in info modules.
         ),
     )
 
-    client = arguments.get_sensu_client(module.params['auth'])
+    client = arguments.get_sensu_client(module.params["auth"])
     path = utils.build_core_v2_path(
-        module.params['namespace'], 'handlers', module.params['name'],
+        module.params["namespace"], "pipelines", module.params["name"],
     )
-    payload = arguments.get_mutation_payload(
-        module.params, 'command', 'filters', 'mutator', 'timeout',
-        'runtime_assets', 'secrets',
-    )
-    payload['type'] = 'pipe'
-    if module.params['env_vars']:
-        payload['env_vars'] = utils.dict_to_key_value_strings(module.params['env_vars'])
-
     try:
-        changed, handler = utils.sync(
-            module.params['state'], client, path, payload, module.check_mode,
-            do_differ,
-        )
-        module.exit_json(changed=changed, object=handler)
+        handlers = utils.prepare_result_list(utils.get(client, path))
     except errors.Error as e:
         module.fail_json(msg=str(e))
+
+    module.exit_json(changed=False, objects=handlers)
 
 
 if __name__ == '__main__':
