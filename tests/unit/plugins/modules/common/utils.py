0b373dbf219827ad300f3dd09bde7cdb1219dbde
rakibhossainctr <rakib.hossain.ctr@sumologic.com>
2025-09-11 16:18:02 +0530

    fixing missing ansible profile for unit tests
    Signed-off-by: rakibhossainctr <rakib.hossain.ctr@sumologic.com>

--- tests/unit/plugins/modules/common/utils.py
+++ tests/unit/plugins/modules/common/utils.py
@@ -2,14 +2,24 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 import json
+import os
 
 from ansible.module_utils import basic
 from ansible.module_utils._text import to_bytes
 
 from mock import patch
 
+# Set the serialization profile before any AnsibleModule operations
+# Use the legacy profile that works with older Ansible versions
+basic._ANSIBLE_PROFILE = 'legacy'
+os.environ['ANSIBLE_SERIALIZATION_PROFILE'] = 'legacy'
+
 
 def set_module_args(**args):
+    # Ensure serialization profile is set
+    basic._ANSIBLE_PROFILE = 'legacy'
+    os.environ['ANSIBLE_SERIALIZATION_PROFILE'] = 'legacy'
+    
     if '_ansible_remote_tmp' not in args:
         args['_ansible_remote_tmp'] = '/tmp'
     if '_ansible_keep_remote_files' not in args:
